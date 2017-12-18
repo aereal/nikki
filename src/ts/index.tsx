514bdb19cb6ad559527bebe6ae6714b4252fd83a
aereal <aereal@aereal.org>
2017-12-18 23:09:40 +0900

    Extract NewArticlePageComponent

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -3,13 +3,8 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { postArticle } from "./actions/articles";
-import { AuthenticationComponent } from "./components/authentication";
-import { EditorComponent } from "./components/editor";
-import { Article } from "./models/article";
-import { AuthedUser } from "./models/user";
 import { EditArticlePageComponent, Props as EditArticlePageComponentProps } from "./pages/editArticle";
-import { SignInComponent } from "./presentations/signIn";
+import { NewArticlePageComponent, Props as NewArticlePageComponentProps } from "./pages/newArticle";
 
 function getInitialProps<T>(): T | null {
   const rawInitialProps = document.body.dataset.initialProps;
@@ -20,38 +15,14 @@ function getInitialProps<T>(): T | null {
   return initialProps as T;
 }
 
-interface RootProps {
-  authedUser: AuthedUser | null;
-}
-class RootComponent extends React.PureComponent<RootProps, {}> {
-  public render(): React.ReactNode {
-    const authedUser = this.props.authedUser
-    const onSubmit = authedUser === undefined || authedUser === null ?
-      () => {} :
-      (article: Article) => {
-        postArticle(authedUser, article).then((postedArticle) => {
-          console.log(postedArticle);
-        });
-        alert("publish");
-      };
-    const newArticle = { body: "", title: "" };
-    return (
-      <AuthenticationComponent
-        authenticated={() => this.props.authedUser !== null }
-        authenticatedView={<EditorComponent headerHeight="10vh" onSubmit={onSubmit} article={newArticle} />}
-        authenticationView={<SignInComponent />} />
-    );
-  }
-}
-
 const Router: React.SFC<{ location: Location }> = ({ location }) => {
   switch (location.pathname) {
     case "/":
-      const rootProps = getInitialProps<RootProps>();
+      const rootProps = getInitialProps<NewArticlePageComponentProps>();
       if (rootProps === null) {
         throw new Error("Invalid initial props");
       }
-      return (<RootComponent {...rootProps} />);
+      return (<NewArticlePageComponent {...rootProps} />);
     default:
       if (location.pathname.match(/^\/articles\/\d+/)) {
         const props = getInitialProps<EditArticlePageComponentProps>();
