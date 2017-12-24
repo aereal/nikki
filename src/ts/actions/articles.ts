da8730d397c104977ce3fa1b0a582cc64982c388
aereal <aereal@aereal.org>
2017-12-24 22:34:25 +0900

    Post an article using GraphQL

--- src/ts/actions/articles.ts
+++ src/ts/actions/articles.ts
@@ -2,14 +2,31 @@ import { API_ORIGIN } from "../endpoints";
 import { Article, isPostedArticle , PostedArticle } from "../models/article";
 import { AuthedUser } from "../models/user";
 
+const postArticleMutation = `
+mutation post($article: ArticleInputType!) {
+  postArticle(article: $article) {
+    id
+    title
+    body
+  }
+}
+`;
+
 export const postArticle = (author: AuthedUser, article: Article): Promise<PostedArticle> => {
-  const req = window.fetch(`${API_ORIGIN}/articles`, {
-    body: JSON.stringify({
+  const variables = {
+    article: {
       body: article.body,
       title: article.title,
+    },
+  };
+  const req = window.fetch(`${API_ORIGIN}/graphql`, {
+    body: JSON.stringify({
+      query: postArticleMutation,
+      variables,
     }),
     credentials: "same-origin",
     headers: {
+      "content-type": "application/json",
       "visitor-key": author.authKey,
     },
     method: "POST",
