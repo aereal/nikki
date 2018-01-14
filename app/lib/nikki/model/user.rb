475e175f5116f115409abd06d2715b06ad965a48
aereal <aereal@aereal.org>
2018-01-14 18:07:46 +0900

    Remove auth_key

--- app/lib/nikki/model/user.rb
+++ app/lib/nikki/model/user.rb
@@ -1,13 +1,12 @@
 module Nikki
   module Model
     class User
-      attr_reader :id, :name, :slug, :auth_key
+      attr_reader :id, :name, :slug
 
-      def initialize(id: , name: , slug: , auth_key: )
+      def initialize(id: , name: , slug: )
         @id = id
         @name = name
         @slug = slug
-        @auth_key = auth_key
       end
     end
   end
