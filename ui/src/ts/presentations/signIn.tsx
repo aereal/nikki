84c8754e48c3f3ca61ff379dd9faa75801844e9e
aereal <aereal@aereal.org>
2018-01-14 18:02:08 +0900

    alert

--- ui/src/ts/presentations/signIn.tsx
+++ ui/src/ts/presentations/signIn.tsx
@@ -25,7 +25,7 @@ export const SignInComponent: React.SFC<{}> = () => {
           }}
           onFailure={(res) => {
             console.log(res);
-            debugger;
+            alert("failed to login");
           }}
         />
       </div>
