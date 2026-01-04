934eaa43e9f39a53f81dc42c093a80c7edc0b9cc
aereal <trasty.loose@gmail.com>
2026-01-04 22:28:05 +0900

    feat(public): pnpm create vue

---
+++ public/prettier.config.mjs
@@ -0,0 +1,14 @@
+/**
+ * @type {import('prettier').Config}
+ */
+const config = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+}
+
+export default config
