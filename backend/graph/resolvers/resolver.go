5e6dd9488a2be1676a14d416807be4780c5308de
aereal <trasty.loose@gmail.com>
2026-01-26 12:08:07 +0900

    fix(backend): gqlgen generate

---
+++ backend/graph/resolvers/resolver.go
@@ -0,0 +1,20 @@
+package resolvers
+
+// THIS CODE WILL BE UPDATED WITH SCHEMA CHANGES. PREVIOUS IMPLEMENTATION FOR SCHEMA CHANGES WILL BE KEPT IN THE COMMENT SECTION. IMPLEMENTATION FOR UNCHANGED SCHEMA WILL BE KEPT.
+
+import (
+	"context"
+
+	"github.com/aereal/nikki/backend/graph/dto"
+	"github.com/aereal/nikki/backend/graph/exec"
+)
+
+type Resolver struct{}
+
+func (r *queryResolver) Article(ctx context.Context, slug string) (*dto.Article, error) {
+	panic("not implemented")
+}
+
+func (r *Resolver) Query() exec.QueryResolver { return &queryResolver{r} }
+
+type queryResolver struct{ *Resolver }
