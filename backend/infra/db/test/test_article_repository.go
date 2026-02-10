163db683d0782625d61cae50562abe3eaee0d5c4
aereal <trasty.loose@gmail.com>
2026-02-10 14:11:44 +0900

    test(backend): generate queries for testing

--- backend/infra/db/test/test_article_repository.go
+++ backend/infra/db/test/test_article_repository.go
@@ -1,12 +1,17 @@
 package test
 
 import (
+	"context"
+	"database/sql"
+
 	"github.com/aereal/nikki/backend/domain"
 	"github.com/aereal/nikki/backend/infra/db"
+	"github.com/aereal/nikki/backend/infra/db/test/queries"
 )
 
-func provideTestArticleRepository(_ *provisionedDB, r *db.ArticleRepository, articleIDGen db.IDGenerator[domain.ArticleID], articleRevisionIDGen db.IDGenerator[domain.ArticleRevisionID]) *TestArticleRepository {
+func provideTestArticleRepository(rawdb *provisionedDB, r *db.ArticleRepository, articleIDGen db.IDGenerator[domain.ArticleID], articleRevisionIDGen db.IDGenerator[domain.ArticleRevisionID]) *TestArticleRepository {
 	return &TestArticleRepository{
+		rawdb:                      rawdb.db,
 		ArticleRepository:          r,
 		ArticleIDGenerator:         articleIDGen,
 		ArticleRevisionIDGenerator: articleRevisionIDGen,
@@ -16,6 +21,13 @@ func provideTestArticleRepository(_ *provisionedDB, r *db.ArticleRepository, art
 type TestArticleRepository struct {
 	*db.ArticleRepository
 
+	rawdb                      *sql.DB
 	ArticleIDGenerator         db.IDGenerator[domain.ArticleID]
 	ArticleRevisionIDGenerator db.IDGenerator[domain.ArticleRevisionID]
+}
+
+type ReviseArticleParam = queries.ReviseArticleParams
+
+func (r *TestArticleRepository) Revise(ctx context.Context, params []ReviseArticleParam) error {
+	return queries.New(r.rawdb).BulkReviseArticle(ctx, params)
 }
